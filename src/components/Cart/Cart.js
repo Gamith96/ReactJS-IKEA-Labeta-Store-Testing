ea2769e389dbbf2fdcb08b069cecfe8991328bd3
Gamith96 <gamithki@gmail.com>
2020-02-29 23:33:32 +0530

    React JS Project Test

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Title from '../Title';
+import CartCoulmns from './CartCoulmns';
+import EmptyCart from './EmptyCart';
+import {ProductConsumer} from '../../context';
+import CartList from './CartList';
+import CartTotals from './CartTotals';
+
+export default class Cart extends Component {
+    render() {
+        return (
+            <section>
+                <ProductConsumer>
+                    {value => {
+                        const {cart} = value;
+                        if(cart.length > 0 ){
+                            return(
+                                <React.Fragment>
+                                <Title name="your" title="cart"/>
+                                <CartCoulmns/>
+                                <CartList value={value}/>
+                                <CartTotals value={value}/>
+                                </React.Fragment>
+                            );
+                        }
+                        else{
+
+                            return <EmptyCart />;
+
+                        }
+                    }}
+
+                </ProductConsumer>
+               
+                </section>
+        )
+    }
+}
---
+++ src/data.js
@@ -0,0 +1,122 @@
+export const storeProducts = [{
+
+  id: 1,
+  title: "TACKAN Soap dispenser, white",
+  img: "img/product-1.png",
+  price: 490,
+  company:"IKEA",
+  info:
+  "Easy to refill as the dispenser has a wide opening.",
+  inCart: false,
+  count:0,
+  total:0 
+
+
+  
+  
+},
+{
+id: 2,
+title: "RINNIG Soap dispenser, grey, 450 ml",
+img: "img/product-2.png",
+price: 890,
+company:"IKEA",
+info:
+"Keep the sink area neat and tidy with this soap dispenser. The modern, minimalist design makes it perfect to match with products in the same series. Sometimes a small change makes a big difference.",
+inCart: false,
+count:0,
+total:0 
+
+},
+
+{
+    id: 3,
+    title: "PRUTA Food container, set of 17, transparent, green",
+    img: "img/product-3.png",
+    price: 1950,
+    company:"IKEA",
+    info:
+    "A good set of basic food containers for everything from cold cuts, cheese etc. to leftovers and individual food portions.Several empty food containers can be stacked inside one another to save space in your cabinets.Reduce food waste by storing your leftovers in a food container and reheating for another meal.The transparent jar makes it easy to find what you are looking for, regardless of where it is placed.Several food containers can be stacked on top of one another to save space in fridge and freezer.Comprises: 4 containers (9x9x4cm, 150ml), 3 containers (12x8x4cm, 150ml), 2 containers (12x12x4cm, 300ml), 2 containers (12x12x14cm, 1l), 2 containers (14x14x6cm, 600ml), 2 containers (14x14x16cm, 1,6l) and 2 containers (23x16x8cm, 1,8l).Wash this product before using it for the first time.This container is not spill-proof and may leak if you store liquid contents like soup in it. If you think there is a risk of leakage, do not place the container in for example a bag.",
+    inCart: false,
+    count:0,
+    total:0  
+},
+{
+
+    id: 4,
+    title: "PRUTA Food container, transparent, yellow, 0.6 l",
+    img: "img/product-4.png",
+    price: 800,
+    company:"IKEA",
+    info:
+    "Suitable for storing everything from cold cuts, cheese etc. to individual portions of food.The transparent jar makes it easy to find what you are looking for, regardless of where it is placed.Several empty food containers can be stacked inside one another to save space in your cabinets.Several food containers can be stacked on top of each other to save space in the fridge and cabinets.",
+    inCart: false,
+    count:0,
+    total:0 
+
+},
+{
+    id: 5,
+    title: "NYSKÖLJD Dish drying mat, dark grey, 44x36 cm",
+    img: "img/product-5.png",
+    price: 980,
+    company:"IKEA",
+    info:
+    "This drying mat is an easy and quick way for you to get more drying space for your dishes.The drying mat can also be placed under a dish drainer to soak up water that drips down from the drainer.Easy to store by folding it or hanging it on a hook with the loop.Place under small appliances like a mixer or a blender to protect the underlying surface from scratches and marks.",
+    inCart: false,
+    count:0,
+    total:0 
+
+},
+{
+    id: 6,
+    title: "STOMMA Wall Clock",
+    img: "img/clock.jpg",
+    price: 850,
+    company:"IKEA",
+    info:
+    "Great clock",
+    inCart: false,
+    count:0,
+    total:0 
+},
+{
+    id: 7,
+    title: "Knife Sharpner",
+    img: "img/knife.jpg",
+    price: 2500,
+    company:"IKEA",
+    info:
+    "Easy to refill as the dispenser has a wide opening.",
+    inCart: false,
+    count:0,
+    total:0 
+},
+{
+    id: 8,
+    title: "BORRIS Carpet",
+    img: "img/dws.jpg",
+    price: 760,
+    company:"IKEA",
+    info:
+    "Easy to refill as the dispenser has a wide opening.",
+    inCart: false,
+    count:0,
+    total:0 
+}
+
+
+
+];
+export const detailProduct = {
+    id: 1,
+  title: "TACKAN Soap dispenser, white",
+  img: "img/product-1.png",
+  price: 490,
+  company:"IKEA",
+  info:
+  "Easy to refill as the dispenser has a wide opening.",
+  inCart: false,
+  count:0,
+  total:0 
+};
